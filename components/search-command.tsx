d203e26e2160e9949ba7480d78668126c917bd54
asura69 <you@example.com>
2024-03-31 23:12:22 +0530

    final

--- components/search-command.tsx
+++ components/search-command.tsx
@@ -17,7 +17,6 @@ import {
 import { useSearch } from "@/hooks/use-search";
 import { api } from "@/convex/_generated/api";
 
-
 export const SearchCommand = () => {
   const { user } = useUser();
   const router = useRouter();
@@ -66,7 +65,7 @@ export const SearchCommand = () => {
               key={document._id}
               value={`${document._id}-${document.title}`}
               title={document.title}
-              onSelect={onSelect}
+              onSelect={() => onSelect(document._id)}
             >
               {document.icon ? (
                 <p className="mr-2 text-[18px]">
